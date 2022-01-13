a59d942ca99558ec64c10f161d05b037fc2ad105
tnichlani <tnichlani@github.com>
2022-01-13 15:39:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import numpy as np
+import cv2
+import os
+import re
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+
+
+col_frames = os.listdir('frames/')
+
+col_frames.sort(key=lambda f: int(re.sub('\D', '', f)))
+
+col_images = []
+
+for i in col_frames:
+    img = cv2.imread('frames/'+i)
+    col_images.append(img)
+
+
+i = 13
+
+for frame in [i, i+1]:
+    plt.imshow(cv2.cvtColor(col_images[frame], cv2.COLOR_BGR2RGB))
+    plt.title("frame: " +str(frame))
+    plt.show()
+
+
+grayA = cv2.cvtColor(col_images[i], cv2.COLOR_BGR2RGB)
+grayB = cv2.cvtColor(col_images[i+1], cv2.COLOR_BGR2RGB)
+
+#plot difference
+
+plt.imshow(cv2.absdiff(grayB, grayA), cmap='gray')
+plt.show()
+
+diff_image = cv2.absdiff(grayB, grayA)
+
+ret, thresh = cv2.threshold(diff_image, 30, 255, cv2.THRESH_BINARY)
+
+plt.imshow(thresh, cmap= 'gray')
+plt.show()
+
+kernel = np.ones((3,3), np.uint8)
+dilated = cv2.dilate(thresh, kernel, iterations=1)
+
+plt.imshow(dilated, cmap = 'gray')
+plt.show()
+
+# plot line 
+plt.imshow(dilated)
+cv2.line(dilated, (0, 80), (256, 80), (100,0,0))
+plt.show()
+
+#find contours
+
+contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+valid_cntrs = []
+
+for i, cntr in enumerate(contours):
+    x,y,w,h = cv2.boundingRect(cntr)
+    if (x <= 200) & (y >= 80) & (cv2.contourArea(cntr) >= 25):
+        valid_cntrs.append(cntr)
+
+
+len(valid_cntrs)
+
+dmy = col_images[13].copy()
+
+cv2.drawContours(dmy, valid_cntrs, -1, (127, 200, 0), 2)
+cv2.line(dmy, (0, 80), (256, 80), (100, 255, 255))
+plt.imshow(dmy)
+plt.show()
+
+
